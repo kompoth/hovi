b4671a5cab60133656a39981cc862e85866dd56f
Negrebetskiy <kmiziz.com@gmail.com>
2022-05-21 13:10:11 +0300

    fancy str

--- tables.py
+++ tables.py
@@ -16,3 +16,6 @@ class Tile(Base):
 
     def __repr__(self):
         return f"<{self.ftype}:{self.public_id}:{self.name}>"
+
+    def __str__(self):
+        return f"{self.public_id}. {self.name} ({self.ftype}), {self.source}"
